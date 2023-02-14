11ab0bc9657c59074a8a0e635451306aebb0489d
Foziljon-2000 <120250186+Foziljon-2000@users.noreply.github.com>
2023-02-14 16:50:21 +0500

    task1

---
+++ task1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("")
+	fmt.Println("Hello golang")
+	fmt.Println("My name is Foziljon")
+	fmt.Println("I love _______, not sure yet")
+	fmt.Println("")
+}
